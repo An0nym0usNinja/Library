eb9a1502bd4d076a87895e2e8874ae9ae2f46b08
Adriaan Mouton <arouxmouton@gmail.com>
2024-12-13 10:59:38 +0200

    Deleted old laravel instance

--- resources/views/components/primary-button.blade.php
+++
@@ -1,6 +0,0 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'focus:outline-none text-white bg-indigo-700
-    hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg
-    text-sm px-5 py-2.5 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700
-    dark:focus:ring-indigo-900']) }}>
-    {{ $slot }}
-</button>
--- routes/models/book.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Generate resource routes for the given controller.
- */
-
-use Illuminate\Support\Facades\Route;
-
-Route::resource('books', 'App\Http\Controllers\BookController');
